4c2d18ff08ce65af96e59da29a682adab3173f2a
Khang Hoang <hoangtrieukhang@gmail.com>
2016-01-01 18:14:22 +0700

    add tests

--- loadDefaultValues.js
+++ loadDefaultValues.js
@@ -2,22 +2,33 @@ var Promise = require('bluebird');
 var fs = require('fs');
 var path = require('path');
 
-var filePath = path.resolve(__dirname, './defaults.json');
+function getDefaultValues(pathString) {
+    pathString = pathString || './defaults.json';
+    var filePath = path.resolve(__dirname, pathString);
 
-function getDefaultValues() {
-  return new Promise(function(resolve, reject) {
-    fs.readFile(filePath, 'utf8', function(err, data) {
-      if(err) {
-        reject(err);
-      }
+    return new Promise(function(resolve, reject) {
+        fs.readFile(filePath, 'utf8', function(err, data) {
+            if(err) {
+                reject(err);
+            }
 
-      if(!data) {
-        reject(new Error('No defaults.json is found!!!'));
-      }
+            if(!data) {
+                reject(new Error('No defaults.json is found!!!'));
+            }
 
-      resolve(JSON.parse(data));
+            try {
+                var content = JSON.parse(data);
+            } catch (e) {
+                var error = new Error('Default values is malformed');
+            }
+
+            if (error) {
+                reject(error);
+            }
+
+            resolve(content);
+        });
     });
-  });
 }
 
 module.exports = getDefaultValues;
--- test/index.js
+++ test/index.js
@@ -1,1 +1,36 @@
+var sinon = require('sinon');
+var expect = require('chai').expect;
 
+var getDefaultValues = require('../loadDefaultValues');
+
+describe('Load default values', function() {
+    it('should raise error when there is no default values', function(done) {
+        getDefaultValues('./the-file-that-doesnt-exist.js')
+        .catch(function(e) {
+            expect(e).to.be.not.null;
+            done();
+        });
+    });
+
+    it('should load successfully', function(done) {
+        getDefaultValues('./test/defaults-test.js')
+        .then(function(defaultValues) {
+            expect(defaultValues.foo).to.equal('bar');
+            done();
+        })
+    });
+
+    it('should raise error when loading malformed data', function(done) {
+        getDefaultValues('./test/malformed-data.test.js')
+        .catch(function(e) {
+            expect(e.message).to.equal('Default values is malformed');
+            done();
+        })
+    });
+});
+
+describe('Adding word', function() {
+});
+
+describe('Login', function() {
+});
